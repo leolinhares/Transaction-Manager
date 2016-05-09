a3f24c7192fdcd7e9670b2fa0e5f55ec2536500e
Thiago <thiagoisaiasbrg@gmail.com>
2016-05-09 16:20:12 -0300

    Comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,14 @@ public class Main {
 
 
     public void menu(){
-
+        //retorna uma action sobre uma transacao
+        //retorna um par (transacao,action) ?
+        //Chama a Tr_Graph.search
     }
 
     public void printAll(ArrayList<Transaction> list){
         //Imprimir o estado de todas as transacoes
+        //Verificar onde a lista de transacoes vai ser usada/modificada
     }
 
     public static void main(String[] args) {
